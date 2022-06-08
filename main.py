173335bd033c72325742f41f88a23891dde662be
Victor Augusto <vaugusto@lanum.com.br>
2022-06-08 08:24:14 -0300

    Create main.py
    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# visualização dos dados em python
+import matplotlib.pyplot as plt
+
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+# Título
+plt.title("Meu gráfico em Python")
+
+# Eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+plt.plot(x, y)
+plt.show()
